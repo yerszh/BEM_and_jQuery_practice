5f931df8053c85d2b195b1b07a55aeb7a8665af5
yerszh <yers.zh@gmail.com>
2023-04-06 11:00:40 +0500

    makerange.go

---
+++ makerange.go
@@ -0,0 +1,13 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return nil
+	} else {
+		result := make([]int, max-min)
+		for i := 0; i < max-min; i++ {
+			result[i] = min + i
+		}
+		return result
+	}
+}
