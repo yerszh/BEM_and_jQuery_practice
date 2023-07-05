ec0a512abe0d6e9bbab0fae7617a7a8b614834c1
yerszh <yers.zh@gmail.com>
2023-07-05 14:00:40 +0500

    btreeapplybylevel.go

---
+++ btreeapplybylevel.go
@@ -0,0 +1,22 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root != nil {
+		queue := []*TreeNode{root}
+
+		for len(queue) > 0 {
+			node := queue[0]
+			queue = queue[1:]
+
+			_, _ = f(node.Data)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+}
